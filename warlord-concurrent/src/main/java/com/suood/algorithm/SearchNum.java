82a4354c8913386bd7f50a211ad9ee8e9ffe4870
suguangqiang <mysgq1@gmail.com>
2017-03-11 18:34:51 +0800

    查找

---
+++ warlord-concurrent/src/main/java/com/suood/algorithm/SearchNum.java
@@ -0,0 +1,65 @@
+package com.suood.algorithm;
+
+import com.google.common.collect.Ordering;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by FENGCUIJIE on 2017/3/11.
+ */
+public class SearchNum {
+    static  Integer [] array = {77 ,34 ,91 ,71 ,84 ,50 ,29 ,87 ,93 ,68};
+    //查找最大值
+   Integer searchMax(Integer [] array){
+       int max = 0;
+       for (int i = 0; i < array.length; i++) {
+           if (max==0){
+               max = array[i];
+           }else if (max<array[i]){
+               max =array[i];
+           }else {
+               continue;
+           }
+       }
+       return max;
+    }
+
+    /**
+     * 查找最小值
+     * @param array
+     * @return
+     */
+    Integer searchMin(Integer [] array){
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max==0){
+                max = array[i];
+            }else if (max>array[i]){
+                max =array[i];
+            }else {
+                continue;
+            }
+        }
+        return max;
+    }
+
+    Integer searchTargetMax(Integer [] array,int target  ){
+        Ordering<Integer> ordering = new Ordering<Integer>() {
+            @Override
+            public int compare(Integer left, Integer right) {
+                return right-left;
+            }
+        };
+        List<Integer> list = Arrays.asList(array);
+       return   ordering.natural().greatestOf(list,target).get(target-1);
+
+    }
+
+    public static void main(String[] args) {
+        SearchNum searchNum = new SearchNum();
+        System.out.println(searchNum.searchTargetMax(SearchNum.array,7));
+        System.out.println( searchNum.searchMax(SearchNum.array));
+        System.out.println( searchNum.searchMin(SearchNum.array));
+    }
+}
