f84f74f0cacef4d0cfa553cdea06982ba56ce253
suguangqiang <mysgq1@gmail.com>
2017-03-11 18:40:33 +0800

    求平均值

--- warlord-concurrent/src/main/java/com/suood/algorithm/Calculate.java
+++ warlord-concurrent/src/main/java/com/suood/algorithm/Calculate.java
@@ -1,7 +1,9 @@
 package com.suood.algorithm;
 
+import com.google.common.math.DoubleMath;
+import com.google.common.math.Stats;
+
 import java.util.Arrays;
-import java.util.OptionalDouble;
 
 /**
  * Created by FENGCUIJIE on 2017/3/11.
@@ -9,12 +11,15 @@ import java.util.OptionalDouble;
 public class Calculate {
     static  int [] array = {77 ,34 ,91 ,71 ,84 ,50 ,29 ,87 ,93 ,68};
 
-    double average(int[]arrayOfInt){
+    double averageJDK8(int[]arrayOfInt){
         return  Arrays.stream(arrayOfInt).average().getAsDouble();
     }
-
+    double averageGuava(int[]arrayOfInt){
+        return Stats.meanOf(array);
+    }
     public static void main(String[] args) {
         Calculate calculate = new Calculate();
-        System.out.println(calculate.average(Calculate.array) );
+        System.out.println(calculate.averageJDK8(Calculate.array) );
+        System.out.println(calculate.averageGuava(Calculate.array) );
     }
 }
