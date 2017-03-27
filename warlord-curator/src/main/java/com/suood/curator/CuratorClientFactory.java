2a22466972a33d5ac794c8da4ab36ecd67cd90b4
suguangqiang <mysgq1@gmail.com>
2017-03-27 23:59:12 +0800

    更新提交

--- warlord-curator/src/main/java/com/suood/curator/CuratorClientFactory.java
+++ warlord-curator/src/main/java/com/suood/curator/CuratorClientFactory.java
@@ -3,6 +3,9 @@ package com.suood.curator;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.listen.Listenable;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import java.util.concurrent.TimeUnit;
@@ -41,10 +44,30 @@ public class CuratorClientFactory {
     public static void main(String[] args)  {
         String connectionString = "118.190.40.207:2181,139.129.227.18:2181,114.215.16.32:2181";
         CuratorFramework client =  CuratorClientFactory.getSimpleClient(connectionString);
+        client.getConnectionStateListenable().addListener(new ConnectionStateListener(){
+            @Override
+            public void stateChanged(CuratorFramework client, ConnectionState state){
+                if (state == ConnectionState.LOST) {
+                    //连接丢失
+                    System.out.println("lost session with zookeeper");
+                } else if (state == ConnectionState.CONNECTED) {
+                    //连接新建
+                    System.out.println("connected with zookeeper");
+                } else if (state == ConnectionState.RECONNECTED) {
+                    System.out.println("reconnected with zookeeper");
+                    //连接重连
+//                    for(ZkStateListener s:stateListeners){
+//                        s.reconnected();
+//                    }
+                }
+            }
+        });
         client.start();
+
         System.out.println(client.getState());
         client.getChildren();
-        client.close();
+
 
     }
+
 }
