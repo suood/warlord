3d252177ff6c368a724af192609cee04df1c8620
Alexander <mysgq1@gmail.com>
2018-12-20 01:17:39 +0800

    添加新的工程

---
+++ warlord-springboot/src/main/java/com/suood/warlord/warlordspringboot/WarlordSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.suood.warlord.warlordspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarlordSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarlordSpringbootApplication.class, args);
+	}
+
+}
+
