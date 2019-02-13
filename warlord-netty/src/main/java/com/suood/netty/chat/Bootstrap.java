fbc7b3572ab5ef7320e14e70e93e010c237d4435
Lyn <james_gosling@163.com>
2019-02-13 22:26:25 +0800

    提交netty相关代码

---
+++ warlord-netty/src/main/java/com/suood/netty/chat/Bootstrap.java
@@ -0,0 +1,9 @@
+package com.suood.netty.chat;
+
+//Netty Server 启动类
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ warlord-netty/src/main/java/com/suood/netty/heartbeat/Bootstrap.java
@@ -0,0 +1,9 @@
+package com.suood.netty.heartbeat;
+
+//Netty Server 启动类
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
