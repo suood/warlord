33442af0b8c783286d37645f59352450844a7ef8
Alexander <mysq1@gmail.com>
2019-11-16 01:48:00 +0800

    提交代码

--- learn-reactor/src/main/java/io/pivotal/literx/Part04Transform.java
+++ learn-reactor/src/main/java/io/pivotal/literx/Part04Transform.java
@@ -14,26 +14,31 @@ public class Part04Transform {
 //========================================================================================
 
 	// TODO Capitalize the user username, firstname and lastname
-	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+public	Mono<User> capitalizeOne(Mono<User> mono) {
+		 return mono.map(u -> new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(e->new User(e.getUsername().toUpperCase(),e.getFirstname().toUpperCase(),e.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		 return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
 		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
+	public static void main(String[] args) {
+		Part04Transform part04Transform = new Part04Transform();
+		Mono<User> m = part04Transform.capitalizeOne(Mono.just(User.JESSE));
+		m.subscribe(System.out::print)  ;
+	}
 }
