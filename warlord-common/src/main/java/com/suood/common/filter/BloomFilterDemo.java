e37659a83edce9168b8eeb8d6cadc60bb65a4193
Alexander <mysq1@gmail.com>
2019-12-06 13:46:18 +0800

    添加将要编写的demo

---
+++ warlord-common/src/main/java/com/suood/common/filter/BloomFilterDemo.java
@@ -0,0 +1,12 @@
+package com.suood.common.filter;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * 2019-12-06
+ */
+public class BloomFilterDemo {
+  
+  BloomFilter bloomFilter = BloomFilter.create(Funnels.byteArrayFunnel(), 100);
+}
