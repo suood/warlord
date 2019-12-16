13631ca69cfc13a8334e52929596483da5013db7
Alexander <mysgq1@gmail.com>
2019-12-16 15:50:09 +0800

    add spring boot 2 netty example

---
+++ warlord-netty/src/main/java/netty/example/NettySpringbootApplication.java
@@ -0,0 +1,15 @@
+package netty.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class NettySpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NettySpringbootApplication.class, args);
+  }
+
+}
--- warlord-netty/src/main/java/netty/example/http/NettyHttpServerDemo.java
+++ warlord-netty/src/main/java/netty/example/http/NettyHttpServerDemo.java
@@ -16,108 +16,97 @@ import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.*;
 
 public class NettyHttpServerDemo {
 
-  //channel
-  private ChannelFuture channel;
-  // event loop   nio or epoll  lt or et ;
-  private final EventLoopGroup masterGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
+    //channel
+    private ChannelFuture channel;
+    // event loop   nio or epoll  lt or et ;
+    private final EventLoopGroup masterGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
 
-  private final EventLoopGroup slaveGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
+    private final EventLoopGroup slaveGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
 
-  public void start() // #1
-  {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdown()));
+    public void start() // #1
+    {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdown()));
 
-    try {
-      // #3
-      final ServerBootstrap bootstrap = new ServerBootstrap()
-              .group(masterGroup, slaveGroup)
-              .channel(Epoll.isAvailable() ? EpollServerSocketChannel.class:NioServerSocketChannel.class)
-              .childHandler(new ChannelInitializer<SocketChannel>() // #4
-              {
-                @Override
-                public void initChannel(final SocketChannel ch) {
-                  ch.pipeline().addLast("codec", new HttpServerCodec());
-                  ch.pipeline().addLast("aggregator",
-                      new HttpObjectAggregator(512 * 1024));
-                  ch.pipeline().addLast("request",
-                      new ChannelInboundHandlerAdapter() // #5
-                      {
+        try {
+            // #3
+            final ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(masterGroup, slaveGroup)
+                    .channel(Epoll.isAvailable() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() // #4
+                    {
                         @Override
-                        public void channelRead(ChannelHandlerContext ctx, Object msg)
-                            throws Exception {
-                          if (msg instanceof FullHttpRequest) {
-                            final FullHttpRequest request = (FullHttpRequest) msg;
-                            final String responseMessage = "Hello from Netty!";
-                            FullHttpResponse response = new DefaultFullHttpResponse(
-                                HttpVersion.HTTP_1_1,
-                                HttpResponseStatus.OK,
-                                copiedBuffer(responseMessage.getBytes())
-                            );
-                            if (HttpHeaders.isKeepAlive(request)) {
-                              response.headers().set(
-                                  HttpHeaders.Names.CONNECTION,
-                                  HttpHeaders.Values.KEEP_ALIVE
-                              );
-                            }
-                            response.headers().set(HttpHeaderNames.CONTENT_TYPE,
-                                "text/plain");
-                            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,
-                                responseMessage.length());
+                        public void initChannel(final SocketChannel ch) {
+                            ch.pipeline().addLast("codec", new HttpServerCodec());
+                            ch.pipeline().addLast("aggregator",
+                                    new HttpObjectAggregator(512 * 1024));
+                            ch.pipeline().addLast("request",
+                                    new ChannelInboundHandlerAdapter() // #5
+                                    {
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg)
+                                                throws Exception {
+                                            if (msg instanceof FullHttpRequest) {
+                                                final FullHttpRequest request = (FullHttpRequest) msg;
+                                                final String responseMessage = "Hello from Netty!";
+                                                FullHttpResponse response = new DefaultFullHttpResponse(
+                                                        HttpVersion.HTTP_1_1,
+                                                        HttpResponseStatus.OK,
+                                                        copiedBuffer(responseMessage.getBytes())
+                                                );
+                                                if (HttpUtil.isKeepAlive(request)) {
+                                                    response.headers().set(
+                                                            HttpHeaderNames.CONNECTION,
+                                                            HttpHeaderNames.KEEP_ALIVE
+                                                    );
+                                                }
+                                                response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+                                                response.headers().set(HttpHeaderNames.CONTENT_LENGTH, responseMessage.length());
+                                                ctx.writeAndFlush(response);
+                                            } else {
+                                                super.channelRead(ctx, msg);
+                                            }
+                                        }
 
-                            ctx.writeAndFlush(response);
-                          } else {
-                            super.channelRead(ctx, msg);
-                          }
-                        }
+                                        @Override
+                                        public void channelReadComplete(ChannelHandlerContext ctx) {
+                                            ctx.flush();
+                                        }
 
-                        @Override
-                        public void channelReadComplete(ChannelHandlerContext ctx) {
-                          ctx.flush();
+                                        @Override
+                                        public void exceptionCaught(ChannelHandlerContext ctx,
+                                                                    Throwable cause) {
+                                            ctx.writeAndFlush(new DefaultFullHttpResponse(
+                                                    HttpVersion.HTTP_1_1,
+                                                    HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                                                    copiedBuffer(cause.getMessage().getBytes())
+                                            ));
+                                        }
+                                    });
                         }
-
-                        @Override
-                        public void exceptionCaught(ChannelHandlerContext ctx,
-                            Throwable cause) {
-                          ctx.writeAndFlush(new DefaultFullHttpResponse(
-                              HttpVersion.HTTP_1_1,
-                              HttpResponseStatus.INTERNAL_SERVER_ERROR,
-                              copiedBuffer(cause.getMessage().getBytes())
-                          ));
-                        }
-                      });
-                }
-              })
-              .option(ChannelOption.SO_BACKLOG, 128)
-              .childOption(ChannelOption.SO_KEEPALIVE, true);
-      channel = bootstrap.bind(8080).sync();
-    } catch (final InterruptedException e) {
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            channel = bootstrap.bind(8080).sync();
+        } catch (final InterruptedException e) {
+        }
     }
-  }
 
-  public void shutdown() // #2
-  {
-    slaveGroup.shutdownGracefully();
-    masterGroup.shutdownGracefully();
+    public void shutdown() // #2
+    {
+        slaveGroup.shutdownGracefully();
+        masterGroup.shutdownGracefully();
 
-    try {
-      channel.channel().closeFuture().sync();
-    } catch (InterruptedException e) {
+        try {
+            channel.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    new NettyHttpServerDemo().start();
-  }
+    public static void main(String[] args) {
+        new NettyHttpServerDemo().start();
+    }
 }
