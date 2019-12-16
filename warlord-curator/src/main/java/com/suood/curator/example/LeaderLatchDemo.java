46d9f89074a7b5ece969b135f2018dd9031faee6
Alexander <mysq1@gmail.com>
2019-12-16 19:26:04 +0800

    提交zookeeper相关的例子

---
+++ warlord-curator/src/main/java/com/suood/curator/example/LeaderLatchDemo.java
@@ -0,0 +1,37 @@
+package com.suood.curator.example;
+
+import com.suood.curator.CuratorClientFactory;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.leader.LeaderLatch;
+
+public class LeaderLatchDemo {
+
+  String connectionString = "";
+  String latchPath = "host:port:serviceName";
+  String serverId = "serverId";
+  CuratorFramework client = CuratorClientFactory.getSimpleClient(connectionString);
+  private LeaderLatch leaderLatch = new LeaderLatch(client, latchPath, serverId);
+
+  LeaderLatchDemo(){
+    try {
+      leaderLatch.addListener();
+      leaderLatch.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+class LeaderLatchListener implements org.apache.curator.framework.recipes.leader.LeaderLatchListener {
+
+  @Override
+  public void isLeader() {
+                  //start start service
+  }
+
+  @Override
+  public void notLeader() {
+
+  }
+}
+
