157b74db89cffc9fc038b833784a31f71305ae41
Alexander <mysq1@gmail.com>
2019-12-26 00:34:42 +0800

    添加 非常重要的注释。

--- warlord-springboot/src/main/java/com/suood/warlord/demo/bean/ComponentConfig.java
+++ warlord-springboot/src/main/java/com/suood/warlord/demo/bean/ComponentConfig.java
@@ -6,11 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan(basePackages={"com.suood.warlord.demo"}, excludeFilters = {
-    @ComponentScan.Filter(type = FilterType.ANNOTATION,value = {Controller.class})} )
+@ComponentScan(basePackages = {"com.suood.warlord.demo"}, excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Controller.class})})
 //    ,@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,value = {XXXX.class}))
 public class ComponentConfig {
 
@@ -19,7 +18,10 @@ public class ComponentConfig {
   @Profile(value = "dev")
   // -Dspring.profiles.active=test|dev|prod 为 dev 时创建
 //      ctx.getEnvironment().setActiveProfiles("test","dev");  创建
-  public HelloKitty helloKitty(){
+// 通过@Profile注解 来根据环境来激活标识不同的Bean
+//  @Profile标识在类上，那么只有当前环境匹配，整个配置类才会生效
+//  @Profile标识在Bean上 ，那么只有当前环境的Bean才会被激活
+  public HelloKitty helloKitty() {
     return new HelloKitty();
   }
 }
