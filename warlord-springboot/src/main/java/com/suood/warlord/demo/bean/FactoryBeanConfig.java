693ee4f86e20ef037426ef034cc05993d450105a
Alexander <mysq1@gmail.com>
2019-12-26 00:13:06 +0800

    提交相关赋值实现

--- warlord-springboot/src/main/java/com/suood/warlord/demo/bean/FactoryBeanConfig.java
+++ warlord-springboot/src/main/java/com/suood/warlord/demo/bean/FactoryBeanConfig.java
@@ -2,10 +2,12 @@ package com.suood.warlord.demo.bean;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan(basePackageClasses = {com.suood.warlord.demo.bean.ProcessGirls.class})
 @ComponentScan(basePackages = {"com.suood.warlord.demo.bean"})
+@PropertySource(value = {"classpath:demo.properties"})
 public class FactoryBeanConfig {
 
 }
--- warlord-springboot/src/main/java/com/suood/warlord/demo/bean/PrettyGirl.java
+++ warlord-springboot/src/main/java/com/suood/warlord/demo/bean/PrettyGirl.java
@@ -2,18 +2,20 @@ package com.suood.warlord.demo.bean;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrettyGirl implements InitializingBean , DisposableBean {
-
+  @Value("${girl.name}")
+  private String name;
   @Override
   public void destroy() throws Exception {
-    
+    System.out.println(this.name + " destroy !");
   }
 
   @Override
   public void afterPropertiesSet() throws Exception {
-
+    System.out.println("gils's name is " + this.name);
   }
 }
