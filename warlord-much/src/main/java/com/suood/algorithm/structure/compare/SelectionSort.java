1c9408757626f96e97d3026d0b46196461dfcc6d
Alexander <mysgq1@gmail.com>
2020-03-25 00:40:27 +0800

    提交选择排序

--- warlord-much/src/main/java/com/suood/algorithm/structure/compare/SelectionSort.java
+++ warlord-much/src/main/java/com/suood/algorithm/structure/compare/SelectionSort.java
@@ -1,16 +1,37 @@
 package com.suood.algorithm.structure.compare;
 
-import java.util.Random;
+/**
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+ * <p>
+ * 选择排序的主要优点与数据移动有关。如果某个元素位于正确的最终位置上，则它不会被移动。 选择排序每次交换一对元素，它们当中至少有一个将被移到其最终位置上， 因此对{\displaystyle
+ * n}n个元素的表进行排序总共进行至多{\displaystyle n-1}{\displaystyle n-1}次交换。 在所有的完全依靠交换去移动元素的排序方法中，选择排序属于非常好的一种。
+ */
+public class SelectionSort extends AbstractSort {
 
-public class SelectionSort {
-  int [] intArray ;
-  public SelectionSort( int t ){
-    t= t<10?10:t;
-    this.intArray = new int [t];
-    Random random = new Random();
-    for (int i = 0; i < t; i++) {
-      intArray[i] =random.nextInt(1024);
+  public SelectionSort(int t) {
+    super(t);
+  }
+
+  @Override
+  void doSort() {
+    for (int i = 0; i < intArray.length - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < intArray.length; j++) {
+        if (intArray[min] > intArray[j]) {
+          min = j;
+        }
+      }
+      int temp = intArray[i];
+      intArray[i] = intArray[min];
+      intArray[min] = temp;
     }
   }
 
+  public static void main(String[] args) {
+
+    SelectionSort selectionSort = new SelectionSort(150);
+    selectionSort.printOrigin();
+    selectionSort.doSort();
+    selectionSort.printOrigin();
+  }
 }
